ac31981772ae0d0555ab328eecf4fdbdbf018913
romanakash <romanakash2000@gmail.com>
2019-12-15 15:14:24 +0000

    interceptor

--- __tests__/main.js
+++ __tests__/main.js
@@ -1,3 +1,5 @@
+const esmImport = require("esm")(module);
+
 describe("Hello", () => {
   test("adds 1 + 1 = 2", () => {
     expect(1 + 1).toEqual(2);
--- src/cli.js
+++ src/cli.js
@@ -2,14 +2,18 @@ import commander from "commander";
 import axios from "axios";
 import fs from "fs";
 
+const API = axios.create({
+  baseURL: "http://example.com"
+});
+
+API.interceptors.request.use(config => {
+  return config;
+});
+
 const program = new commander.Command();
 
 program.parse(process.argv);
 
-export function cli() {
+export function cli(args) {
   console.log(program.args);
 }
-
-const API = axios.create({
-  baseURL: "http://example.com"
-});
